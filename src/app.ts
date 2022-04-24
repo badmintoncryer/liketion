f1be99a02896fd5128b10837f2bd68f4bfff9aa0
kazuho cryershinozuka <cryershinozuka.kb@om.asahi-kasei.co.jp>
2022-04-24 22:01:48 +0900

    [added] rootパスの設定

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,37 @@
+const rootDir = require("app-root-path");
 const express = require("express");
 
 const getHello = require("./api/getHello");
 
-const app = express();
+interface loadSettingsReturn {
+  port: number;
+  rootPath: string;
+}
+// yamlから設定をロード
+const loadSettings = (): loadSettingsReturn => {
+  const fs = require("fs");
+  const yaml = require("js-yaml");
+  const yamlText = fs.readFileSync(`${rootDir}/config/settings.yaml`, "utf8");
+  return yaml.load(yamlText);
+};
 
-app.get("/", getHello);
+interface getRootUrlProps {
+  port: number;
+  rootPath: string;
+}
+const getRootUrl = (settings: getRootUrlProps): string => {
+  return settings.rootPath || "/";
+};
+// ルートパスの設定
+let rootUrl = "/";
+try {
+  rootUrl = getRootUrl(loadSettings());
+} catch (error) {
+  console.error(error);
+}
+
+// ルーティング
+const app = express();
+app.get(rootUrl, getHello);
 
 module.exports = app;
