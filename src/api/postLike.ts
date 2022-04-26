2e175a6c7dc3e1f953c40ca006b7fa78849e2a47
kazuho cryershinozuka <cryershinozuka.kb@om.asahi-kasei.co.jp>
2022-04-26 23:10:19 +0900

    [added] type

--- src/api/postLike.ts
+++ src/api/postLike.ts
@@ -6,7 +6,7 @@ const db = require("../database/create");
 // TODO 同一の名前が登録されている場合、登録を行わない
 const postLike = (req: Request, res: Response): void => {
   const contentId: string = req.params.id;
-  const name = req.body.name;
+  const name: string = req.body.name;
   const stmt = db.prepare("insert into likes (contentId,name) values (?,?)");
   stmt.run(contentId, name, (error: { message: string }) => {
     if (error) {
