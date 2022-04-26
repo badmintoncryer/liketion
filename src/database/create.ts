809ed101e799da7a71d58e17263e5d33d45cc5d6
kazuho cryershinozuka <cryershinozuka.kb@om.asahi-kasei.co.jp>
2022-04-26 23:39:23 +0900

    [removed] test data insertion

--- src/database/create.ts
+++ src/database/create.ts
@@ -6,8 +6,6 @@ const db = new sqlite3.Database("./db/liketion.sqlite3", (error: { message: stri
     console.error("database error: " + error.message);
   } else {
     db.serialize(() => {
-      //都度table削除（あれば）
-      db.run("drop table if exists likes");
       //table生成（無ければ）
       db.run(
         "create table if not exists likes( \
@@ -18,11 +16,6 @@ const db = new sqlite3.Database("./db/liketion.sqlite3", (error: { message: stri
         (error: { message: string }) => {
           if (error) {
             console.error("table error: " + error.message);
-          } else {
-            //初期データinsert
-            db.run("insert into likes(contentId,name) values(?,?)", "hoge", "kazuho");
-            db.run("insert into likes(contentId,name) values(?,?)", "foo", "kazuho");
-            db.run("insert into likes(contentId,name) values(?,?)", "hoge", "mizuki");
           }
         }
       );
